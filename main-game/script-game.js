ab1ec771209d44777cddd876be086bd052df5fd8
fauzan.gahara <fauzanfg20@gmail.com>
2022-07-24 02:19:55 +0700

    function done, no class

--- main-game/script-game.js
+++ main-game/script-game.js
@@ -1,49 +1,107 @@
-const bot_list = ["rock","paper","scissors"];
+const bot_list = ["batu","kertas","gunting"];
 let bot_sample = ""
 let user_sample = ""
 let user_hand_batu = document.getElementById("batu")
 let user_hand_gunting = document.getElementById("gunting")
 let user_hand_kertas = document.getElementById("kertas")
 let center_item = document.getElementById("center-item")
-
+let refresh = document.getElementById("refresh-button")
+let addElement1 = [...document.getElementsByClassName("player-hand")]
 const comparator = (user_sample,bot_sample) =>{
     if(user_sample === bot_sample){
         console.log('Tie')
+        
+        center_item.innerHTML = `<img style="z-index: 1; position: absolute; margin-left: -150px;"src="game-assets/Rectangle 73.svg" alt=""><img style="z-index: 2; position: absolute;margin-left: -55px; margin-top: 80px;" src="game-assets/DRAW.svg" alt="">`
     }
-    else if(user_sample == 'rock'){
-        if(bot_sample == 'paper'){
+    else if(user_sample == 'batu'){
+        if(bot_sample == 'kertas'){
             console.log('Kalah')
+            center_item.innerHTML = `<img style="z-index: 1; position: absolute; margin-left: -150px;"src="game-assets/Rectangle 67.svg" alt=""><img style="z-index: 2; position: absolute;margin-left: -55px; margin-top: 80px;" src="game-assets/COM WIN.svg" alt="">`
         }else{
             console.log('Menang')
+            
+            center_item.innerHTML = `<img style="z-index: 1; position: absolute; margin-left: -150px;"src="game-assets/Rectangle 67.svg" alt=""> <img style="z-index: 2; position: absolute;margin-left: -95px; margin-top: 40px;" src="game-assets/player win.svg" alt="">`
         }
     }
-    else if(user_sample == 'scissors'){
-        if(bot_sampler == 'rock'){
+    else if(user_sample == 'gunting'){
+        if(bot_sample == 'batu'){
             console.log('Kalah')
+            center_item.innerHTML = `<img style="z-index: 1; position: absolute; margin-left: -150px;"src="game-assets/Rectangle 67.svg" alt=""><img style="z-index: 2; position: absolute;margin-left: -55px; margin-top: 80px;" src="game-assets/COM WIN.svg" alt="">`
         }else{
             console.log('Menang')
+            
+            center_item.innerHTML = `<img style="z-index: 1; position: absolute; margin-left: -150px;"src="game-assets/Rectangle 67.svg" alt=""> <img style="z-index: 2; position: absolute;margin-left: -95px; margin-top: 40px;" src="game-assets/player win.svg" alt="">`
         }
     }
-    else if(user_sample == 'paper'){
-        if(bot_sample == 'scissors'){
+    else if(user_sample == 'kertas'){
+        if(bot_sample == 'gunting'){
             console.log('Kalah')
+            center_item.innerHTML = `<img style="z-index: 1; position: absolute; margin-left: -150px;"src="game-assets/Rectangle 67.svg" alt=""><img style="z-index: 2; position: absolute;margin-left: -55px; margin-top: 80px;" src="game-assets/COM WIN.svg" alt="">`
         }else{
             console.log('Menang')
+            
+            center_item.innerHTML = `<img style="z-index: 1; position: absolute; margin-left: -150px;"src="game-assets/Rectangle 67.svg" alt=""> <img style="z-index: 2; position: absolute;margin-left: -95px; margin-top: 40px;" src="game-assets/player win.svg" alt="">`
         }
     }
     console.log(`pilihan player: ${user_sample}`)
     console.log(`pilihan bot: ${bot_sample}`)
 }
-
-user_hand_batu.addEventListener("click", () =>{
-    bot_sample = bot_list[Math.floor(Math.random()*bot_list.length)];
-    comparator("rock",bot_sample)
-})
-user_hand_gunting.addEventListener("click", () =>{
-    bot_sample = bot_list[Math.floor(Math.random()*bot_list.length)];
-    comparator("scissors",bot_sample)
-})
-user_hand_kertas.addEventListener("click", () =>{
-    bot_sample = bot_list[Math.floor(Math.random()*bot_list.length)];
-    comparator("paper",bot_sample)
-})
+const kotak_bot = (bot_sample) =>{
+    switch (bot_sample) {
+        case "batu":
+            document.querySelector('svg #kotak-batu-bot[fill]').setAttribute("fill","#C4C4C4")
+            break;
+        case "kertas":
+            document.querySelector('svg #kotak-kertas-bot[fill]').setAttribute("fill","#C4C4C4")
+            break;
+        case "gunting":
+            document.querySelector('svg #kotak-gunting-bot[fill]').setAttribute("fill","#C4C4C4")
+            break;
+    }
+    addElement1.forEach(addElement3 => {
+        addElement3.setAttribute("style", "cursor: not-allowed;pointer-events: none;")
+    })
+}
+const gamerun = () => {
+    //bot_sample = bot_list[Math.floor(Math.random()*bot_list.length)]
+    user_hand_kertas.addEventListener("click", () =>{
+        bot_sample = bot_list[Math.floor(Math.random()*bot_list.length)];
+        comparator("kertas",bot_sample)
+        document.querySelector(`svg #kotak-kertas-pl[fill]`).setAttribute("fill","#C4C4C4")
+        kotak_bot(bot_sample)
+        // addElement1.forEach(addElement3 => {
+        //     addElement3.setAttribute("style", "cursor: not-allowed;pointer-events: none;")
+        // })
+    })
+    user_hand_batu.addEventListener("click", () =>{
+        bot_sample = bot_list[Math.floor(Math.random()*bot_list.length)];
+        comparator("batu",bot_sample)
+        document.querySelector('svg #kotak-batu-pl[fill]').setAttribute("fill","#C4C4C4")
+        kotak_bot(bot_sample)
+        // addElement1.forEach(addElement3 => {
+        //     addElement3.setAttribute("style", "cursor: not-allowed;pointer-events: none;")
+        // })
+    })
+    user_hand_gunting.addEventListener("click", () =>{
+        bot_sample = bot_list[Math.floor(Math.random()*bot_list.length)];
+        comparator("gunting",bot_sample)
+        document.querySelector('svg #kotak-gunting-pl[fill]').setAttribute("fill","#C4C4C4")
+        kotak_bot(bot_sample)
+        // addElement1.forEach(addElement3 => {
+        //     addElement3.setAttribute("style", "cursor: not-allowed;pointer-events: none;")
+        // })
+    })
+    refresh.addEventListener("click", () =>{
+        center_item.innerHTML = `<img style="margin-top: 90px;"src="game-assets/VS.svg" alt=""></img>`
+        document.querySelector('svg #kotak-gunting-pl[fill]').setAttribute("fill","#9C835F")
+        document.querySelector('svg #kotak-batu-pl[fill]').setAttribute("fill","#9C835F")
+        document.querySelector('svg #kotak-kertas-pl[fill]').setAttribute("fill","#9C835F")
+        document.querySelector('svg #kotak-gunting-bot[fill]').setAttribute("fill","#9C835F")
+        document.querySelector('svg #kotak-batu-bot[fill]').setAttribute("fill","#9C835F")
+        document.querySelector('svg #kotak-kertas-bot[fill]').setAttribute("fill","#9C835F")
+        addElement1.forEach(addElement3 => {
+            addElement3.removeAttribute("style", "cursor: not-allowed;pointer-events: none;")
+        })
+    })
+}
+gamerun()
