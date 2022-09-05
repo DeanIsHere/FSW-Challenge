362a6a9495f2d03c501682e40289ba5acdeedaa2
fauzan.gahara <fauzanfg20@gmail.com>
2022-09-05 08:26:06 +0700

    fc

--- JS/retrieveData.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-
-const retrieveAllData = (source) => {
-  return JSON.parse(fs.readFileSync(source, 'utf-8'))
-}
-
-module.exports = {
-    retrieveAllData
-  }
--- index.js
+++ index.js
@@ -1,71 +1,12 @@
-// initialize package
 const express = require('express')
+const bodyParser = require('body-parser');
+const { User, Biodatas } = require('./models')
+
 const app = express()
-const fs = require('fs')
-const bodyParser = require('body-parser')
-const path = require('path')
-const md5 = require('md5')
-const { retrieveAllData } = require('./JS/retrieveData')
-
-
 const jsonParser = bodyParser.json()
-// set view enginer
-app.set('view engine','ejs')
-// introduce static file
-app.use('/CSS', express.static(__dirname+'/CSS'))
-app.use('/JS', express.static(__dirname+'/JS'))
-app.use('/game-assets', express.static(__dirname+'/game-assets'))
-app.use('/assets', express.static(__dirname+'/assets'))
 
-// read user data from json
-app.get('/', function (req, res) {
-  res.status(404).send(retrieveAllData('user.json')) 
-  })
-// user login
-app.post('/login', jsonParser, (req, res) => {
-  let my_user = retrieveAllData('user.json')
-  let checker = false
-    for (let i =0; i < my_user.length; i++){   
-      if(my_user[i].password === md5(md5(req.body.password)) && my_user[i].user_name === (req.body.user_name)){
-        checker = true
-        console.log(`${i+1}-${checker}`)
-      } 
-    }
-  if(checker){
-    res.send("Authorized")
-  } else {
-    res.status(401).send("Unauthorized")
-  }
-})
-// main page
-app.get('/main-page', (req, res)=>{
-  res.render("main-page")
-})
-// game page
-app.get('/game-page', (req, res)=>{
-  res.render("game-page")
-})
-// login page
-app.get('/login-page',(req,res)=>{
-  res.render("login-page")
-})
-//information page
-app.get('/user-list-page',(req,res)=>{
-  res.render("user-list-page")
-})
-// user details page
-app.get('/user-details-page',(req,res)=>{
-  res.render("user-details-page")
-})
-// user add page
-app.get('/user-add-page',(req,res)=>{
-  res.render("user-add-page")
-})
+app.use('/css', express.static(__dirname+'/css'))
+app.use('/js', express.static(__dirname+'/js'))
+app.set('view engine', 'ejs')
 
-//CREATE
-
-// PORT
-app.listen(8000, () =>{
-  console.log("Application is running at localhost:8000")
-})
-
+//VIEW
