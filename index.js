abad2cfa546edf411d53bdb4b7d6279c9010b55b
fauzan.gahara <fauzanfg20@gmail.com>
2022-09-21 18:23:42 +0700

    authorization done

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express = require('express')
 const passport = require('passport')
 require('./utils/passport')
 const apiRouter = require('./routes/api')
+const pageRouter = require('./routes/page')
 
 const app = express()
 const port = 8989
 
+app.set('view engine', 'ejs')
 app.use(passport.initialize())
 
 app.use(apiRouter)
+app.use(pageRouter)
 
 app.listen(port, () => {
   console.log(`App is running at port ${port}`)
